11b3c5d31c96242878aa5371db4774a1269e3971
almostearthling <franz.g@infinito.it>
2016-01-05 19:19:46 +0100

    Add graphics and start main window design

---
+++ share/when-wizard/modules/wizard.py
@@ -0,0 +1,17 @@
+# file: share/when-wizard/modules/wizard.py
+# -*- coding: utf-8 -*-
+#
+# Implement the Wizard interface
+# Copyright (c) 2015-2016 Francesco Garosi
+# Released under the BSD License (see LICENSE file)
+
+from gi.repository import GLib, Gio
+from gi.repository import GObject
+from gi.repository import Gtk
+from gi.repository import Pango
+
+
+
+
+
+# end.
