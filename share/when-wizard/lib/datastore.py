f0807321fcf091e8015e34ec4f24e6ad37b7a97f
almostearthling <franz.g@infinito.it>
2016-01-30 01:33:05 +0100

    Fix datastore issues

--- share/when-wizard/lib/datastore.py
+++ share/when-wizard/lib/datastore.py
@@ -15,23 +15,23 @@ class PicklingDatastore(object):
         self._data = {}
         self.filename = None
 
-    def _write(self):
-        if self.filename is not None:
-            with open(filename, 'wb') as f:
-                pickle.dump(self._data, f)
-
-    def _read(self):
-        if self.filename is not None:
-            with open(filename, 'b') as f:
-                self._data = pickle.load(f)
-
     def __iter__(self):
         for k in self._data:
             yield k
 
+    def save(self):
+        if self.filename is not None:
+            with open(self.filename, 'wb') as f:
+                pickle.dump(self._data, f)
+
+    def load(self):
+        if self.filename is not None:
+            with open(self.filename, 'rb') as f:
+                self._data = pickle.load(f)
+
     def get(self, unique_id):
         if not self._data:
-            self._read()
+            self.load()
         if unique_id in self._data:
             return self._data[unique_id]
         else:
@@ -39,11 +39,11 @@ class PicklingDatastore(object):
 
     def put(self, unique_id, value):
         self._data[unique_id] = value
-        self._write()
+        self.save()
 
     def remove(self, unique_id):
         del self._data[unique_id]
-        self._write()
+        self.save()
 
 
 # end.
